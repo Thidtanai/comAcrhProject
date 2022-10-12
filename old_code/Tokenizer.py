9d62a597b180435fc0ee30f3cedf5671932e63eb
thidtanai <thidtanai_k@cmu.ac.th>
2022-10-12 22:44:02 +0700

    change_Oldparser&tokenizerTo old_code

---
+++ old_code/Tokenizer.py
@@ -0,0 +1,34 @@
+#
+#def tokenizer(src) :
+#    result = src.split()
+#    print(result)
+#
+#
+### test 
+#f = open("inputTest.txt", "r")
+#code = f.read()
+#print(f.read())
+#tokenizer(code)
+
+class Tokenizer :
+
+    def __init__(self, src, next, pos):
+        self.src = src
+        self.pos = 0
+        computeNext()
+
+    def  computeNext():
+        print("compute")
+    
+    def peek():
+        print("peek")
+
+    def peek(str):
+        print(str)
+
+    def consume():
+        print("consume")
+
+    def consume(str):
+        print(str)
+        
