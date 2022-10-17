3c20e7eec9eadf7404ba1e8cfcc527d4b518a719
Thidtanai <thidsloth@gmail.com>
2022-10-18 04:47:49 +0700

    bumb_riscToken_makeStarterFunction

--- Tokenizer/riscToken.py
+++ Tokenizer/riscToken.py
@@ -8,9 +8,46 @@ DIGITS =  '0123456789'
     ERROR
 """
 
+class Error:
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        result = f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        return result
+
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
 """ 
     POSITION
 """
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col += 0
+        
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 """ 
     TOKENS
@@ -22,11 +59,55 @@ TT_RD       = 'RD'
 TT_RS1      = 'RS1'
 TT_RS2      = 'RS2'
 
+class Token:
+    def __init__(self, type, value=None):
+        self.type = type
+        self.value = value
+        
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
 
 """ 
     LEXER
 """
 
+class Lexer:
+    def __init__(self, fn, text):
+        self.fn = fn
+        self.text = text
+        self.pos = Position(-1, 0, -1, fn, text)
+        self.current_char = None
+        self.advance()
+        
+    def advance(self):  # for moving current char to next char.
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+        
+        while self.current_char != None:
+            if self.current_char in '\t':   # check if space or blank skip them.
+                self.advance()
+            elif self.current_char == 'add':
+                tokens.append(Token(TT_FUNC))
+                self.advance()  
+            elif self.current_char in DIGITS:
+                pass
+            else:
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+        
+        return tokens, None
+        
 """ 
     RUN
 """
+def run(fn, text):
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+        
+    return tokens, error
